ac7356ebc9352c8f45be5a8d03de52509e4d1927
Martin Sebera <xmartin.sebera@gmail.com>
2026-06-10 13:17:49 +0200

    Swagger

--- Cbd.Api/Program.cs
+++ Cbd.Api/Program.cs
@@ -1,13 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
