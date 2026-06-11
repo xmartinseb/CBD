375c055e57d618a9be5af28c13e5d3b4dbd66bb0
Martin Sebera <xmartin.sebera@gmail.com>
2026-06-11 22:52:18 +0200

    logger

--- Cbd.Api/Controllers/OrderController.cs
+++ Cbd.Api/Controllers/OrderController.cs
@@ -9,7 +9,7 @@ namespace Cbd.Api.Controllers;
 [ApiController]
 [Route("[controller]")]
 [EnableRateLimiting("default")]
-public sealed class OrderController(CreatedOrdersChannel createdOrdersQueue, IOrdersRepository ordersRepository)
+public sealed class OrderController(CreatedOrdersChannel createdOrdersQueue, IOrdersRepository ordersRepository, ILogger<OrderController> logger)
     : ControllerBase
 {
     [HttpPost("Accept", Name = "AcceptNewOrders")]
@@ -17,8 +17,18 @@ public sealed class OrderController(CreatedOrdersChannel createdOrdersQueue, IOr
     {
         foreach (var order in newOrders)
         {
-            await ordersRepository.AddAsync(order, cancellationToken);
-            await createdOrdersQueue.EnqueueOrderAsync(new OrderCreated(order, DateTime.UtcNow), cancellationToken);
+            try
+            {
+                await ordersRepository.AddAsync(order, cancellationToken);
+                await createdOrdersQueue.EnqueueOrderAsync(new OrderCreated(order, DateTime.UtcNow), cancellationToken);
+                logger.LogInformation("New order registered: Product {p}, Quantity={q}", order.ProductId, order.Quantity);
+            }
+            // TODO: zachytit pouze chyby souvisejici s jednim orderem (povolit partial failure).
+            // Např. při výpadku databáze nechat spadnout celý http request (full failure)
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Storing an order failed");
+            }
         }
 
         return Ok();
